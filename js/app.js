7e466d327a5d7545564d607cc5bb8b1c5bf8318c
Roberto Cervantes <robert.hct@gmail.com>
2020-06-01 21:15:09 -0500

    Crear funcón de botón del formulario

---
+++ js/app.js
@@ -0,0 +1,63 @@
+// Variables
+const max = new Date().getFullYear();
+const min = max - 20;
+const formulario = document.getElementById('cotizar-seguro');
+const selectAnios = document.getElementById('anio');
+const marca = document.getElementById('marca');
+const anio = document.getElementById('anio');
+
+//Event listeners
+
+
+(() => {
+  
+  formulario.addEventListener('submit', enviarDatos);
+  
+  for (let i = max; i > min; i--) {
+    let option = document.createElement('option');
+    option.value = i;
+    option.innerHTML = i;
+    selectAnios.appendChild(option);
+  }
+})();
+
+
+
+//Funciones
+function enviarDatos(e) {
+  e.preventDefault();
+
+  //leer la marca seleccionada del select
+  const marcaSleccionada = marca.options[marca.selectedIndex].value;
+  
+  //lee el valor del año
+  const anioSeleccionado = anio.options[anio.selectedIndex].value;
+  
+  //lee el valor del tipo de seguro
+  const tipoSeleccionado = document.querySelector('input[name="tipo"]:checked').value;
+  ;
+  //Crear instancia de interfaz
+  const interfaz = new Interfaz();
+
+  //revisar que los campos no estén vacios
+  if (marcaSleccionada === '' || anioSeleccionado === '' || tipoSeleccionado === '') {
+    //Interfaz, imprime un error
+    interfaz.mostrarError('Faltan datos, revisa el formulario e intenta de nuevo', 'error');
+  } else {
+    //Instanciar y mostrar interfaz
+  }
+}
+
+
+
+//Constructor para seguro
+function Seguro(marca, anio, tipo) {
+  this.marca = marca;
+  this.anio = anio;
+  this.tipo = tipo;
+}
+
+//Todo lo que se muestra
+function Interfaz() { }
+
+//Mensaje que imprime en el HTML
